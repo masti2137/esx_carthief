36566cfa0c80d6cfc55dde76eff947e0007909a1
masti2137 <95970398+masti2137@users.noreply.github.com>
2021-12-11 15:15:00 +0100

    Add files via upload

---
+++ locales/en.lua
@@ -0,0 +1,13 @@
+Locales['en'] = {
+  ['vehicle_robbery'] = 'Kradziez Pojazdu!',
+  ['mission_failed'] = 'Kradziez nieudana!',
+  ['get_back_car_10s'] = 'Masz 10 sekund na powrot do auta',
+  ['get_back_car_1m'] = 'Masz 1 minute na powrot do auta',
+  ['car_provided_rule'] = 'Musisz skorzystać z samochodu, który ci udostępniono i musisz całkowicie się zatrzymać.',
+  ['already_robbery'] = 'Już trwa napad na samochód!',
+  ['not_enough_cops'] = 'W miescie nie ma wystarczajacej ilosci policjantow',
+  ['car_stealing_in_progress'] = 'Trwa kradzież samochodów. Śledzenie pojazdów będzie aktywne na twoim radarze',
+  ['steal_a_car'] = 'Nacisnij ~INPUT_CONTEXT~ zeby ukrasc pojazd',
+  ['drop_car_off'] = 'Nacisnij ~INPUT_CONTEXT~ aby opiscic pojazd',
+  ['cooldown'] = 'Niedawno dokonano napadu na pojazd. Poczekaj %s sekund aby ukrasc kolejny'
+}
